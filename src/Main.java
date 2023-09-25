12e1373b91dac4a2fb16caf0eac31b2044163a6e
Bruno <bruno.luis.999@hotmail.com>
2023-09-25 15:51:54 -0300

    Teste

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bem-vindo ao Screen Match");
+
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int ano = 2023;
+
+        System.out.println("Ano de lançamento: " + ano);
+
+        double media = 2.5 + 3.5 + 5.5 / 3;
+
+        System.out.println("Nota média: " + media);
+
+        int classificacao = (int) (media / 2);
+
+        System.out.println("Classificação: " + classificacao);
+
+        String dados = """
+              Aspas triplas
+              da para escrever em mais de uma linha
+              legal, né ? :)
+                """;
+
+    }
+
+
+}
